d224f0d29df8ffad3571cd609f7f3eebe9e9e4bf
Michal Sytko <michal.sytko@gmail.lcom>
2025-03-02 15:00:48 +0100

    update

--- src/editor/TrackElementEditorRenderer.ts
+++ src/editor/TrackElementEditorRenderer.ts
@@ -1,4 +1,4 @@
-import { Scene, Mesh, StandardMaterial, Vector3, MeshBuilder } from '@babylonjs/core';
+import { Scene, Mesh, StandardMaterial, Vector3, MeshBuilder, Color3 } from '@babylonjs/core';
 import { TrackElement } from '../data/types';
 import { TrackElementRenderer } from '../editor/TrackElementRenderer';
 import { TextureManager } from '../manager/TextureManager';
@@ -12,9 +12,11 @@ export class TrackElementEditorRenderer extends TrackElementRenderer {
         
         // Create debug material for editor mode
         this.debugMaterial = new StandardMaterial("debug-material", scene);
-        this.debugMaterial.wireframe = false;
         this.debugMaterial.diffuseTexture = TextureManager.getInstance(scene).getContainerTexture();
+        this.debugMaterial.useAlphaFromDiffuseTexture = true;
         this.debugMaterial.backFaceCulling = false;
+        
+        this.debugMaterial.alpha = 0.4;
     }
 
     public render(position: Vector3): Mesh {
@@ -34,21 +36,8 @@ export class TrackElementEditorRenderer extends TrackElementRenderer {
                         const cube = MeshBuilder.CreateBox("debug-cube", {
                             size: blockSize
                         }, this.scene);
-
-                        const material = new StandardMaterial("cube-material", this.scene);
-                        material.diffuseTexture = TextureManager.getInstance(this.scene).getContainerTexture();
-
-                        // Setze backFaceCulling auf false für alle Würfel an den Außenkanten
-                        const isOuterEdge = (x === 0 || x === this.trackElement.containerSize.x - 1) &&
-                                          (y === 0 || y === this.trackElement.containerSize.y - 1) ||
-                                          (y === 0 || y === this.trackElement.containerSize.y - 1) &&
-                                          (z === 0 || z === this.trackElement.containerSize.z - 1) ||
-                                          (x === 0 || x === this.trackElement.containerSize.x - 1) &&
-                                          (z === 0 || z === this.trackElement.containerSize.z - 1);
-
-                        // Prüfe ob der Würfel an einer äußeren Ecke oder Kante ist
-                        material.backFaceCulling = !isOuterEdge;
-                        cube.material = material;
+                        
+                        cube.material = this.debugMaterial;
                         
                         cube.position = new Vector3(
                             (x - (this.trackElement.containerSize.x - 1) / 2) * blockSize,
@@ -69,9 +58,6 @@ export class TrackElementEditorRenderer extends TrackElementRenderer {
     public dispose(): void {
         super.dispose();
         this.meshes.forEach(mesh => {
-            if (mesh.material) {
-                mesh.material.dispose();
-            }
             mesh.dispose();
         });
         this.meshes = [];
